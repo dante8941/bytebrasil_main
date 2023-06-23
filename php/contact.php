b090dc2837ce56517c6524dc6a3585846b0057cb
Dante Dantas <dantedantasloc@gmail.com>
2023-06-22 23:35:44 -0400

    Primeira versão do site, há um bug no posicionamento das imagens na galeria que irei ajeitar em breve

---
+++ php/contact.php
@@ -0,0 +1,21 @@
+<?php
+// Email Submit
+if ( isset($_POST['email']) && isset($_POST['name']) && isset($_POST['subject']) && isset($_POST['message']) && filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) ) {
+ 
+  // detecta e preveni injeções header
+  $test = "/(content-type|bcc:|cc:|to:)/i";
+  foreach ( $_POST as $key => $val ) {
+    if ( preg_match( $test, $val ) ) {
+      exit;
+    }
+  }
+
+$headers = 'From: ' . $_POST["name"] . '<' . $_POST["email"] . '>' . "\r\n" .
+    'Reply-To: ' . $_POST["email"] . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+
+  //
+  mail( "dantedantasloc@gmail.com", $_POST['subject'], $_POST['message'], $headers );
+ 
+}
+?>
